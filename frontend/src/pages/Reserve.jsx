3e2b7c177ddc27ccc202d31deb62bbc6f146c570
Trần Minh Quang <tranquang22042002@gmail.com>
2024-11-17 23:21:41 +0700

    add link to button

--- frontend/src/pages/Reserve.jsx
+++ frontend/src/pages/Reserve.jsx
@@ -4,20 +4,18 @@ import './Reserve.css';
 const About = () => {
     return (
         <div className="about-page">
-            {/* Hero Section */}
             <section className="hero">
                 <div className="hero-text">
                     <h1>Reserve Your Table, Savor The Moments!</h1>
                 </div>
             </section>
 
-            {/* Content Section */}
             <section className="content">
                 <h2>Experience The Convenience Of Booking Your Table!</h2>
                 <p>
-                At our restaurant, we believe that every dining experience should be seamless and enjoyable. That's why we offer a hassle-free reservation system that allows you to secure your table ahead of time. Whether you're planning a special occasion, a romantic dinner, or a casual gathering with friends, booking your table ensures that you have the perfect spot waiting for you. Simply choose your desired date and time, and let us take care of the rest. Enjoy a memorable meal with exceptional service and delectable dishes, all without the worry of wait times. Reserve your table today and savor the moments with us!
+                    At our restaurant, we believe that every dining experience should be seamless and enjoyable. That's why we offer a hassle-free reservation system that allows you to secure your table ahead of time. Whether you're planning a special occasion, a romantic dinner, or a casual gathering with friends, booking your table ensures that you have the perfect spot waiting for you. Simply choose your desired date and time, and let us take care of the rest. Enjoy a memorable meal with exceptional service and delectable dishes, all without the worry of wait times. Reserve your table today and savor the moments with us!
                 </p>
-                <button>Reserve now!</button>
+                <button onClick={() => window.location.href = '/reservation'}>Reserve now!</button>
             </section>
         </div>
     );
